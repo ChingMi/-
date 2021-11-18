9f28260b3c4cf77835c9c0f536ae93e41c1f3dc8
GoldenRice <bluesky20242@gmail.com>
2021-11-18 16:09:13 +0800

    Initial Commit

---
+++ 電子書練習/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  電子書練習
+//
+//  Created by 黃靖蜜 on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
